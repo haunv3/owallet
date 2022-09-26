05df68ef9d5a54b46176e28c7e91ad3f1992a4d3
sonlha <lhas1307@gmail.com>
2022-09-26 16:48:07 +0700

    resolve feedback

--- packages/cosmos/src/tx-tracer/index.ts
+++ packages/cosmos/src/tx-tracer/index.ts
@@ -61,9 +61,9 @@ export class TendermintTxTracer {
         ? this.wsEndpoint
         : '/' + this.wsEndpoint;
 
-      url = (url + wsEndpoint).replace(/\/\//g, '/');
+      url = url?.endsWith('/') ? url + wsEndpoint.slice(1) : url + wsEndpoint;
 
-      // url = url?.endsWith('/') ? url + wsEndpoint.slice(1) : url + wsEndpoint;
+      console.log('url ===', url);
     }
 
     return url;
@@ -223,7 +223,10 @@ export class TendermintTxTracer {
 
   //  Subscribe the msg
   async subscribeMsgByAddress(msg: string): Promise<any> {
-    await Promise.race([100, this.subscribeMsg(msg)]);
+    const result = await this.subscribeMsg(msg);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(result), 100);
+    });
   }
 
   subscribeMsg(address: string): Promise<any> {
